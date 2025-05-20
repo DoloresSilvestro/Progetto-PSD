33965c9722ab9f5f31f6bcf27766549b2012e483
DoloresSilvestro <dolores.silvestro05@gmail.com>
2025-05-20 18:48:32 +0200

    modifica implementazione header file lezioni.h -Dolores

--- lezioni.h
+++ lezioni.h
@@ -16,23 +16,18 @@ Gli elementi principali sono:
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct Lezione {
-	int id;
 	char nome[30];
-	char giorno[30];
-	int durata;
+	struct tm dataOra;
 	int maxPartecipanti;
 	int partecipantiAttuali;
 	struct Lezione* nodoNext;
 } Lezione;
 
-Lezione* aggiungiLezione(Lezione* testa, int id, const char* nome, const char* giorno, int durata, int maxPartecipanti);
-Lezione* cercaLezione(Lezione* testa, int id);
-void modificaLezione(Lezione* testa, int id, const char* nuovoNome, const char* nuovoGiorno, int nuovaDurata, int nuovoMaxPartecipanti);
-Lezione* rimuoviLezione(Lezione* testa, int id);
-void stampaLezioni(Lezione* testa);
-void liberaLezioni(Lezione* testa);
-int lezioneCompleta(Lezione* lezione);
+Lezione* aggiungiLezione(const char* nome, struct tm dataOra, int maxPartecipanti);
+void modificaLezione(Lezione* testa, const char* nuovoNome, struct tm* nuovoDataOra, int nuovoMaxPartecipanti);
+void liberaLezione(Lezione* testa);
 
 #endif
